b49074a30b8a19b18e4a4a46634d4781a28f8ea3
Sev3r <sjoerdever04@gmail.com>
2025-03-21 12:27:44 +0100

    foutje

--- Script.js
+++ Script.js
@@ -2,18 +2,18 @@ let clicks = 0;
 
 const BarkNoise = document.getElementById("BarkNoise");
 const DogGif = document.getElementById("DogGif.gif");
-        
-
-        function moveGif() {
-            const screenWidth = window.innerWidth;
-            const gifWidth = DogGif.width;
-            const distance = screenWidth - gifWidth;
-            
-            DogGif.style.transform = `translateX(${distance}px)`;
-        }
 
 
-document.getElementById("openPopup").addEventListener("click", function() {
+function moveGif() {
+    const screenWidth = window.innerWidth;
+    const gifWidth = DogGif.width;
+    const distance = screenWidth - gifWidth;
+
+    DogGif.style.transform = `translateX(${distance}px)`;
+}
+
+
+document.getElementById("openPopup").addEventListener("click", function () {
     clicks++;
     BarkNoise.play();
 
@@ -23,12 +23,12 @@ document.getElementById("openPopup").addEventListener("click", function() {
     }
 });
 
-document.getElementById("yes").addEventListener("click", function() {
+document.getElementById("yes").addEventListener("click", function () {
     document.body.classList.toggle("custom-cursor");
     document.getElementById("popup").style.display = "none";
     moveGif();
 });
 
-document.getElementById("no").addEventListener("click", function() {
+document.getElementById("no").addEventListener("click", function () {
     document.getElementById("popup").style.display = "none";
 });
